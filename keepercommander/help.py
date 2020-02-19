0002582e8d4964eb6cb8f78082af5699fa5138c6
MAKINO,Yasukazu <yskz@tutanota.de>
2020-02-20 05:38:32 +0900

    cmd-option Dict : None

--- keepercommander/help.py
+++ keepercommander/help.py
@@ -7,13 +7,12 @@ command_Dict_parser = {k:v.get_parser() for (k,v) in commands.items()}
 command_Dict_option_string = {} # type: Dict[str, set]
 for cmd, psr in command_Dict_parser.items():
     try:
-        command_Dict_option_string[cmd] = {k for k in psr._option_string_actions if k.startswith('--')}
+        command_Dict_option_string[cmd] = {k:None for k in psr._option_string_actions if k.startswith('--')}
     except:
         pass
 
 command_info_keys_completer = NestedCompleter.from_nested_dict(
     command_Dict_option_string
 )
-print()
 # = WordCompleter(command_info_keys)
 
