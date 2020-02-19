2b85f57b88ccfda1f6cbe0b08b506940da57c8f6
MAKINO,Yasukazu <yskz@tutanota.de>
2020-02-19 22:58:22 +0900

    newly added

--- keepercommander/help.py
+++ keepercommander/help.py
@@ -1,13 +1,15 @@
 from .cli import command_info
 from prompt_toolkit.completion import WordCompleter
 from .commands import register_commands, register_enterprise_commands, aliases, commands, enterprise_commands
-
+from collections import deque
 command_info_keys = [k.split('|')[0] for k in command_info.keys()]
 command_Dict_parser = {k:v.get_parser() for (k,v) in commands.items()}
 command_Dict_option_string = {} # type: Dict[str, str]
+for cmd, psr in command_Dict_parser.items():
+    try:
+        command_Dict_option_string[cmd] = [k for k in psr._option_string_actions if k.startswith('--')]
+    except:
+        pass
 
-
-command_Dict_option_string_actions = {k:v._option_string_actions for (k,v) in command_Dict_parser.items()}
-command_info_dict = vars(command_info)
 command_info_keys_completer = WordCompleter(command_info_keys)
 
