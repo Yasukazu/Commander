c9dec8eabc6db495ec2be92cdfd3d6a8afc6171f
MAKINO,Yasukazu <yskz@tutanota.de>
2020-02-23 01:34:42 +0900

    logger addHandler test

--- test_config.py
+++ test_config.py
@@ -1,7 +1,12 @@
 import keepercommander.config as kcfg
 import logging
 
-kcfg.set_by_json_file()
-kcfg.start()
+config_dict = kcfg.set_by_json_file()
+start_dict = kcfg.start()
+logging_handlers = start_dict[kcfg.Logging]
 logger = logging.getLogger(__name__)
-logging.info("Logging from 'test_config.py'.")
+logger.setLevel(logging.INFO)
+for hdr in logging_handlers:
+    logger.addHandler(hdr)
+logger.info("Logger from 'test_config.py'.")
+logging.info(f"Logging from {__name__}")
