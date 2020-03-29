99c3e4c2d56371976f306c383c1b25dc3142c4bb
MAKINO,Yasukazu <yskz@tutanota.de>
2020-03-29 17:58:03 +0900

    logging handler

--- test_as_lib.py
+++ test_as_lib.py
@@ -1,8 +1,16 @@
 import logging
 # logger.basicConfig(filename=f"{__name__}.log")
 logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
-logger.addHandler(logging.FileHandler(f"{__file__}.log"))
+sHandler = logging.StreamHandler()
+sHandler.setLevel(logging.INFO)
+logger.addHandler(sHandler)
+handler = logging.FileHandler(f"{__file__}.log")
+handler.setLevel(logging.INFO)
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# add formatter to ch
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 import sys
 import keepercommander as kc
 from keepercommander import api, params
@@ -49,12 +57,15 @@ with open(f"{__file__}.output", mode='w') as o_f:
                 try:
                     response = request.urlopen(home_url)
                     soup = BeautifulSoup(response, features="html.parser")
-                    rec.title = page_title = soup.title.text
+                    page_title = soup.title.text
                     response.close()
                     print(f"{record_uid}\t{page_title}\t{rec.login_url}", file=o_f)
+                    rec.title = page_title
                     params.sync_data = True
                     api.update_record(params, rec)
                     logger.info(f"Title of {record_uid} is update to {page_title}")
+                    logging.shutdown()
+                    break
                 except error.HTTPError as err:
                     logger.error(f">>>> Web page protocol error: {str(err)}:{err.code} <<<<")
                 except AttributeError as err:
