58452451c53728c2b2133f28b685e1c8bb7c992f
MAKINO,Yasukazu <yskz@tutanota.de>
2020-04-02 20:06:48 +0900

    logging.shutdowm

--- test_as_lib.py
+++ test_as_lib.py
@@ -6,9 +6,7 @@ sHandler.setLevel(logging.INFO)
 logger.addHandler(sHandler)
 handler = logging.FileHandler(f"{__file__}.log")
 handler.setLevel(logging.INFO)
-# create formatter
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# add formatter to ch
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 import sys
@@ -43,13 +41,13 @@ def extract_base(url):
         raise MatchError
     return rt.group(1, 2)
     
-with open(f"{__file__}.output", mode='w') as o_f:
+with open(f"{__file__}.output", mode='a') as o_f:
     for record_uid in params.record_cache:
         rec = api.get_record(params, record_uid)  
         try:
             base_url = extract_base(rec.login_url)
         except (MatchError, IndexError):
-            logger.error(f"Login URL ({rec.login_url}) error at record uid: {record_uid}") # base_url = ('', '')
+            logger.debug(f"Login URL ({rec.login_url}) error at record uid: {record_uid}") # base_url = ('', '')
         else:
             title = rec.title
             home_url = '://'.join(base_url)
@@ -59,16 +57,17 @@ with open(f"{__file__}.output", mode='w') as o_f:
                     soup = BeautifulSoup(response, features="html.parser")
                     page_title = soup.title.text
                     response.close()
-                    print(f"{record_uid}\t{page_title}\t{rec.login_url}", file=o_f)
                     rec.title = page_title
                     params.sync_data = True
                     api.update_record(params, rec)
                     logger.info(f"Title of {record_uid} is update to {page_title}")
-                    logging.shutdown()
-                    break
+                    print(f"{record_uid}\t{page_title}\t{rec.login_url}", file=o_f)
                 except error.HTTPError as err:
-                    logger.error(f">>>> Web page protocol error: {str(err)}:{err.code} <<<<")
+                    logger.info(f">>>> Web page protocol error: {str(err)}:{err.code} <<<<")
                 except AttributeError as err:
-                    logger.error(f">>>> Title error: {str(err)} <<<<")
+                    logger.info(f">>>> Title error: {str(err)} <<<<")
                 except error.URLError as err:
-                    logger.error(f">>>> Login web address access error: {str(err)} <<<<")
+                    logger.info(f">>>> Login web address access error: {str(err)} <<<<")
+                except Exception:
+                    logger.exception("unknown error")
+logging.shutdown()
