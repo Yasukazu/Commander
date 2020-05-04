cd6d88d9c18cc6aaef9c27972d009c634d8a1f87
MAKINO,Yasukazu <yskz@tutanota.de>
2020-05-04 11:37:48 +0000

    newly added

--- list_all_titles.py
+++ list_all_titles.py
@@ -32,6 +32,7 @@ import sqlite3
 from datetime import datetime, timedelta, timezone
 import validators
 from  validators import url as url_validator
+import tldextract
 
 class MatchError(Exception):
     pass
@@ -85,49 +86,52 @@ if __name__ == '__main__':
     extractor = URLExtract()
     __SQLFILE__ = "title-keeper"
     __SQLEXT__ = '.sqlite'
-    TABLE_NAME = 'nomatch'
+    TABLE_NAME = 'invalid_url'
     conn = sqlite3.connect(f"{__SQLFILE__}{__SQLEXT__}",
         detect_types = sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     sqlite3.dbapi2.converters['DATETIME'] = sqlite3.dbapi2.converters['TIMESTAMP']
     cur = conn.cursor()
-    cur.execute(f"CREATE TABLE if not exists {TABLE_NAME} (uid text unique, report text, error integer, updated datetime)")
+    cur.execute(f"CREATE TABLE if not exists {TABLE_NAME} (uid text unique, title text, url text, report text, error integer, updated datetime)")
     jp_tz = timezone(timedelta(hours=+9), 'JST')
     # with open(f"{__file__}.output", mode='a') as o_f:
-    def put_db(uid, report, error):
+    def put_db(uid, title, url, report, error):
         print(report)
-        cur.execute(f"insert into {TABLE_NAME} VALUES (:uid, :report, :error, :updated)", (uid, report, error, datetime.now(jp_tz)) )
+        cur.execute(f"insert into {TABLE_NAME} VALUES (:uid, :title, :url, :report, :error, :updated)", (uid, title, url, report, error, datetime.now(jp_tz)) )
         conn.commit()
     MAX_REPEAT = 9999
     for repeat, uid in enumerate(params.record_cache):
         if repeat >= MAX_REPEAT:
             break
         rec = api.get_record(params, uid)
+        title = rec.title
         login_url = rec.login_url
         msg = "invalid Login URL"
         try:
-            if not url_validator(login_url):
-                put_db(uid, msg, 1)
+            if login_url and not url_validator(login_url):
+                put_db(uid, title, login_url, msg, 1)
                 continue
         except validators.ValidationFailure:
-            put_db(uid, msg, 1)
+            put_db(uid, title, login_url, msg, 1)
             continue
         try:
-            parse_result = urlparse(rec.login_url)
+            login_url_parsed = urlparse(rec.login_url)
         except ValueError: #(MatchError, IndexError):
             msg = "parse error in Login URL"
-            put_db(uid, msg, 2)
+            put_db(uid, title, login_url, msg, 2)
             continue
             # logger.debug( f"Login URL ({rec.login_url}) error at record uid: {record_uid}" )
         else:
             # title = rec.title
-            net_loc = parse_result.netloc.split(':')[0]
+            net_loc = login_url_parsed.netloc.split(':')[0]
+            login_url_domain_suffix = '.'.join(tldextract.extract(login_url_parsed.netloc.split(':')[0])[1:])
             # home_url = '://'.join(base_url)
             urls_in_title = extractor.find_urls(rec.title)
             url_in_title = urls_in_title[0] if urls_in_title else None
             if url_in_title:
-                if url_in_title != net_loc:
-                    msg = "URL in title does not match with login URL!"
-                    put_db(uid, msg, 2)
+                title_url_domain_suffix = '.'.join(tldextract.extract(url_in_title)[1:])
+                if login_url_domain_suffix != title_url_domain_suffix:
+                    msg = "domain in title does not match with login domain!"
+                    put_db(uid, title, login_url, msg, 3)
              #for url_in_title in urls_in_title: # if not title:  # if title is empty
                 # print("No title at record_uid:%s" % record_uid)
                 # res.encoding = res.apparent_encoding()
