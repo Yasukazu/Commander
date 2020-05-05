8aae283fbc2a3e705c67106acbad16e1b5f4d70c
MAKINO,Yasukazu <yskz@tutanota.de>
2020-05-05 08:08:03 +0000

    remove comment lines

--- url_check.py
+++ url_check.py
@@ -13,10 +13,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 import sys
 import os
-#from pathlib import Path
-#file_dir = os.path.dirname(__file__)
-#parent_dir = os.path.join(file_dir, '..')
-#sys.path.append(parent_dir)
+
 import keepercommander as kc
 from keepercommander import api, params
 import chardet
@@ -132,42 +129,6 @@ if __name__ == '__main__':
                 if login_url_domain_suffix != title_url_domain_suffix:
                     msg = "domain in title does not match with login domain!"
                     put_db(uid, title, login_url, msg, 3)
-             #for url_in_title in urls_in_title: # if not title:  # if title is empty
-                # print("No title at record_uid:%s" % record_uid)
-                # res.encoding = res.apparent_encoding()
-                # req = request.Request(home_url)
-                # with request.urlopen(req) as res:
-                #    body = res.read()
-                '''
-                try:
-                    res = requests.get(home_url)
-                    soup = BeautifulSoup(res.text,# body.decode('utf-8')
-                                         features="html.parser") 
-                    page_title = soup.title.text
-                    if not page_title:
-                        print("No page title in webpage:%s" % home_url)
-                        continue
-                    rec.title = page_title
-                    params.sync_data = True
-                    api.update_record(params, rec)
-                    logger.info(
-                        f"Title of {record_uid} is update to {page_title}")
-                    print(f"{record_uid}\t{page_title}\t{rec.login_url}",
-                          file=o_f)
-                except error.HTTPError as err:
-                    logger.info(
-                        f">>>> Web page protocol error: {str(err)}:{err.code} <<<<"
-                    )
-                except AttributeError as err:
-                    logger.info(f">>>> Title error: {str(err)} <<<<")
-                except error.URLError as err:
-                    logger.info(
-                        f">>>> Login web address access error: {str(err)} <<<<"
-                    )
-                except Exception as ex:
-                    logger.exception("unknown error at %s: " % record_uid)
-                    raise
-                    '''
-    # logging.shutdown()
+
     conn.close()
     exit(0) # to suppress warning of 'Exit without exit code'
