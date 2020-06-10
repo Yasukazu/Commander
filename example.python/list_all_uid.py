887ec841e9b1dddab86187b3c1b1fd530a1c4640
MAKINO,Yasukazu <yskz@tutanota.de>
2020-06-11 08:05:59 +0900

    WithParams class adds modified_time

--- example.python/list_all_uid.py
+++ example.python/list_all_uid.py
@@ -3,40 +3,69 @@ import sys
 import os
 import getpass
 import json
+import datetime
+
 sys.path.append("..")  # pwd includes keepercommander"
 #sys.path.append("../.venv/lib/python3.6/dist-packages")
 from keepercommander import api, params # set PYTHONPATH=<absolute path to keepercommander>
 from pprint import pprint
 
-
-class KeeperLogin(object):
+class WithParams(params.KeeperParams):
     ''' Login and sync_down automatically 
         user-ID is gotten from $KEEPER_USER
         user password if from $KEEPER_PASSWORD
         or parameters as with(user, password) '''
-
     USER = 'KEEPER_USER'
     PASSWORD = 'KEEPER_PASSWORD'
+  
+    # from keepercommander.api import login, sync_down
+    
+    def get_modified_timestamp(self, record_uid):
+        current_rec = self.record_cache[record_uid]
+        return current_rec['client_modified_time']
+    
+    def get_modified_time(self, record_uid):
+        return datetime.datetime.fromtimestamp(self.get_modified_timestamp(record_uid) / 1000)
 
+    def login(self, **kwargs):
+        return api.login(self, **kwargs)
+        
+    def sync_down(self):
+        return api.sync_down(self)
+        
     def __enter__(self, user=None, password=None, user_prompt='User:', password_prompt='Password:'):
-        self.params = params.KeeperParams()
-        self.params.user = user or os.getenv(KeeperLogin.USER) or input(user_prompt)
-        self.params.password = password or os.getenv(KeeperLogin.PASSWORD) or getpass.getpass(password_prompt)
-        api.login(self.params)
-        api.sync_down(self.params)
+        self.user = user or os.getenv(WithParams.USER) or input(user_prompt)
+        self.password = password or os.getenv(WithParams.PASSWORD) or getpass.getpass(password_prompt)
+        api.login(self, user=self.user, password=self.password)
+        api.sync_down(self)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.params.clear_session()  # clear internal variables
+        self.clear_session()  # clear internal variables
     
     def get_every_unencrypted(self):
-        for uid, packet in self.params.record_cache.items():
+        for uid, packet in self.record_cache.items():
             yield uid, json.loads(packet['data_unencrypted'].decode('utf-8'))
  
     def get_every_record(self):
-        for uid in self.params.record_cache:
-            yield uid, api.get_record(keeper_login.params, uid) 
+        for uid in self.record_cache:
+            yield uid, api.get_record(self, uid)       
             
+    def get_every_uid(self):
+        for uid in self.record_cache:
+            yield uid
+            
+    def get_all_uids(self):
+        return self.record_cache.keys()
+        
+    import datetime
+    def get_record(self, uid):
+       rec = api.get_record(self, uid).to_dictionary()    
+       dt = datetime.datetime.fromtimestamp(self.record_cache[uid]['client_modified_time'] / 1000)
+       rec['modified_time'] = dt
+       return rec
+
+        
 
 if __name__ == '__main__':
     import logging
@@ -44,24 +73,13 @@ if __name__ == '__main__':
     logger = logging.getLogger(__file__)
     logger.setLevel(logging.INFO)
     inspects = [] # put UIDs to inspect as string literal like 'abc', comma separated 
-    with KeeperLogin() as keeper_login:
-        for uid, unencrypted in keeper_login.get_every_unencrypted():
-            print(f"{uid}:{unencrypted}")
-            break
-            
-        rec_rec = {}
-        uid_date = {}
-        for uid, record in keeper_login.get_every_record():
-            # print(f"\n{uid}:") 
-            rec_rec[uid] = record.to_dictionary()
-            uid_date[uid] = keeper_login.params.get_modified_time(uid)
-            
-        sorted_uid_date = sorted(uid_date.items(), key=lambda kv: kv[1], reverse=True)
-        for k, d in sorted_uid_date:
-            rr = rec_rec[k]
-            rr['modified_time'] = d.isoformat()
+    with WithParams() as keeper_login:
+        rec_list = []
+        for uid in keeper_login.get_all_uids():
+            rec_list.append(keeper_login.get_record(uid))
+        sorted_list = sorted(rec_list, key=lambda r: r['modified_time'], reverse=True)    
+        for rr in sorted_list:
+            rr['modified_time'] = rr['modified_time'].isoformat()
             print(json.dumps(rr, sort_keys=True, indent=4, ensure_ascii=False))
-            # pprint(rr)
-            # print(json.dumps(record.to_dictionary(), sort_keys=True, indent=4))
         
     exit(0)
