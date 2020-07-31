e01949b4c91b0ecd566bc2061bcb4d1b9919b613
MAKINO,Yasukazu <yskz@tutanota.de>
2020-08-01 08:28:00 +0900

    find_duplicated()

--- example.python/login_same_loginurl.py
+++ example.python/login_same_loginurl.py
@@ -18,48 +18,21 @@ def main(user: str, password: str, yesall: bool=False, repeat=0):
    # from operator import attrgetter
    # inspects = [] # put UIDs to inspect as string literal like 'abc', comma separated 
     with KeeperSession(user=user, password=password) as keeper_login:
-        uid_rec_dict = {u:r for (u, r) in keeper_login.get_every_record()}
-        # UPDATE_LIMIT = 2
-        for uid, rec in uid_rec_dict.items():
-            same_login_loginurl_set = defaultdict(set) # Dict[str, Set[str]] {timestamp, set(uid,)} find same login and login_url
-            # same_login_loginurl_dict = {}
-            for vid, rek in uid_rec_dict.items():
-                if vid == uid:
-                    continue
-                if (rec.login == rek.login and
-                    rec.login_url.split('?')[0] == rek.login_url.split('?')[0] and # ignore parameter field of url
-                    ): # rec.timestamp != rek.timestamp):
-                    if len(same_login_loginurl_set) == 0:
-                        same_login_loginurl_set[rec.timestamp].add(uid)
-                    same_login_loginurl_set[rek.timestamp].add(vid)
-            # TODO: remove same timestamp
-            if len(same_login_loginurl_set):
-                from_old_timestamp_list = sorted(same_login_loginurl_set.keys())
-                ts_delete_uid = {ts: same_login_loginurl_set[ts] for ts in from_old_timestamp_list[:-1]}
-                logger.info(f"Dupricating records of older timestamp are going to be deleted: ")
-                for uid_set in ts_delete_uid.values():
+        for timestamp_duplicated_uids in keeper_login.find_duplicated():
+            from_old_timestamp_list = sorted(timestamp_duplicated_uids.keys())
+            to_delete_ts = from_old_timestamp_list[:-1]
+            logger.info(f"Dupricating records of older timestamp are going to be deleted: ")
+            for ts in to_delete_ts:
+                for uid_set in timestamp_duplicated_uids[ts]:
                     for uid in uid_set:
-                        rec = uid_rec_dict[uid]
+                        rec = keeper_login.all_records[uid]
                         logger.info("\t" + pprint.pformat(rec))
                     keeper_login.delete_uids |= uid_set
-                last_timestamp_uid_set = same_login_loginurl_set[from_old_timestamp_list[-1]]
-                if len(last_timestamp_uid_set) > 1:
-                    logger.info(f"last timestamp uid is duplicating: ")
-                    for uid in last_timestamp_uid_set:
-                        logger.info('\t' + uid)
                     
+            if repeat:
                 repeat -= 0
                 if not repeat:
                     break
-
-                # timestamp_order_dict = {k: v for k, v in sorted(same_login_loginurl_set .items(), key=lambda item: item[1])}
-                # for k in timestamp_order_dict:
-                #   keeper_login.delete_records.add(k) # the smallest timestamp record
-                #   update_count += 1
-                #   if len(keeper_login.delete_records) >= UPDATE_LIMIT:
-                #       return
-                #   break
-
     exit(0)
 
 if __name__ == '__main__':
