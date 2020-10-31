c837cb3706a304e68a43a092acf1cdbb304b2c06
MAKINO Yasukazu <yasukazu@pm.me>
2020-10-31 18:38:41 +0900

    made PARSER configargparse object in __init__.py

--- ycommander/__init__.py
+++ ycommander/__init__.py
@@ -22,4 +22,15 @@ __default_locale__ = 'en_US'
 __user_id__ = getenv('KEEPER_USER_ID')
 __pwd__ = getenv('KEEPER_PASSWORD')
 
-CONFIG_FILENAME = getenv('KEEPER_CONFIG') or 'keeper-config.json'
+CONFIG_FILENAME = getenv('KEEPER_CONFIG') or 'keeper.conf'
+CONFIG_PATH = getenv('KEEPER_CONFIG_PATH') or '.'
+from pathlib import Path
+__config_fullpath = Path(CONFIG_PATH) / CONFIG_FILENAME
+import configargparse
+PARSER = configargparse.ArgParser(default_config_files=[__config_fullpath])  # ArgumentParser(prog='keeper', add_help=False)
+PARSER.add('--user', '-ku', dest='user', action='store', env_var='KEEPER_USER', help='Email address for the account.')
+PARSER.add('--password', '-kp', dest='password', action='store', env_var='KEEPER_PASSWORD', help='Master password for the account.')
+
+import logging
+import pylogrus
+logging.setLoggerClass(pylogrus.PyLogrus)
