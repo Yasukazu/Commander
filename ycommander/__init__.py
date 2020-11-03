113436a01af5f72cbfc1786e72ea7197ad776f86
MAKINO Yasukazu <yasukazu@pm.me>
2020-11-03 10:24:27 +0900

    removed configargparse import in __init__.py

--- ycommander/__init__.py
+++ ycommander/__init__.py
@@ -10,9 +10,7 @@
 # Contact: ops@keepersecurity.com
 #
 
-import locale  # for strxfrm sort
 from os import getenv
-import sys, os
 
 __version__ = '4.19'
 __revision__ = "2020-04-26"
@@ -22,14 +20,13 @@ __default_locale__ = 'en_US'
 __user_id__ = getenv('KEEPER_USER_ID')
 __pwd__ = getenv('KEEPER_PASSWORD')
 
+from ycommander import PARSER
+from ycommander.configarg import PARSER
+
 CONFIG_FILENAME = getenv('KEEPER_CONFIG') or 'keeper.conf'
 CONFIG_PATH = getenv('KEEPER_CONFIG_PATH') or '.'
 from pathlib import Path
 __config_fullpath = Path(CONFIG_PATH) / CONFIG_FILENAME
-import configargparse
-PARSER = configargparse.ArgParser(default_config_files=[__config_fullpath])  # ArgumentParser(prog='keeper', add_help=False)
-PARSER.add('--user', '-ku', dest='user', action='store', env_var='KEEPER_USER', help='Email address for the account.')
-PARSER.add('--password', '-kp', dest='password', action='store', env_var='KEEPER_PASSWORD', help='Master password for the account.')
 
 import logging
 import pylogrus
