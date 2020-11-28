c584b5aaaca9a5416cb898207f1712830aa5cf5e
MAKINO,Yasukazu <yskz@tutanota.de>
2020-11-28 09:14:37 +0900

    fields.append save_bitwarden_csv

--- example/keeper_json.py
+++ example/keeper_json.py
@@ -7,16 +7,8 @@ KEEPER_RECORD = ['title', 'login', 'password', 'login_url', 'notes',
 'custom_fields', 'folders']
 KEEPER_SHARED = ['uid', 'path', 'manage_users', 'manage_records', 'can_edit', 'can_share', 'permissions']
 KEEPER_CUSTOM = ["TFC:Keeper"]
-LOGIN_CSV = ["Title",
-    "Username",
-    "Email",
-    "Password",
-    "Website",
-    "TOTP Secret Key",
-    "Custom Field 1",
-    "*Custom Field 2",
-    "Note","Tags" ]
-
+ENPASS_CSV = ["Title", "Username", "Email", "Password", "Website", "TOTP Secret Key", "Custom Field 1", "*Custom Field 2", "Note","Tags" ]
+BITWARDEN_CSV_STR = "folder,favorite,type,name,notes,fields,login_uri,login_username,login_password,login_totp"
 
 class KeeperRecord:
     def __init__(self, dic: Dict):
@@ -35,12 +27,41 @@ def load_keeper_records(fn: str) -> List[Dict]:
         dics = json.load(f)
     return dics['records']
 
-def save_csv(dics: List[Dict], fn: str):
+def expand_s(s: str):
+    return s.replace(r'\n', '\n')
+
+def expand_fields(d: Dict) -> List[str]:
+    lst = [k + ': ' + v for k, v in d.items()]
+    return '\n'.join(lst)
+
+
+def save_bitwarden_csv(dics: List[Dict], fn: str):
+    from attrdict import AttrDict
     with open(fn, 'w') as f:
         wtr = csv.writer(f)
-        wtr.writerow(*LOGIN_CSV)
+        headers = BITWARDEN_CSV_STR.split(',')
+        wtr.writerow(*headers)
+        fields = []
         for dic in dics:
-            wtr.writerow(dic['title'],
+            adic = AttrDict(dic)
+            folder = ''
+            try:
+               for fld in adic.folders:
+                   folder = fld
+                   break
+            except:
+                pass
+            fields.append(folder)
+            fields.append('') # favorite = ''
+            fields.append('login') # type = 'login'
+            fields.append(dic.get('title', ''))
+            fields.append(expand_s(adic.notes))
+            fields.append(expand_fields(adic.fields))
+            fields.append(adic.login_url)
+            fields.append(adic.login)
+            fields.append(adic.password)
+
+            wtr.writerow(
 
 
             )
