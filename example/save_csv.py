59290776267d834e052b33461475377e7183142e
MAKINO,Yasukazu <yskz@tutanota.de>
2020-11-28 20:33:52 +0900

    sanitize removing control chars

--- example/save_csv.py
+++ example/save_csv.py
@@ -16,11 +16,15 @@ KEEPER_SHARED = ['uid', 'path', 'manage_users', 'manage_records', 'can_edit', 'c
 KEEPER_CUSTOM = ["TFC:Keeper"]
 ENPASS_CSV = ["Title", "Username", "Email", "Password", "Website", "TOTP Secret Key", "Custom Field 1", "*Custom Field 2", "Note","Tags" ]
 BITWARDEN_CSV_STR = "folder,favorite,type,name,notes,fields,login_uri,login_username,login_password,login_totp"
+BITWARDEN_FIELDNAMES_DICT = {n:n for n in BITWARDEN_CSV_STR.split(',')}
 
 class ExceedError(ValueError):
     pass
 
 from collections import UserDict
+import unicodedata
+def but_control_char(s):
+    return "".join(ch for ch in s if unicodedata.category(ch)[0]!="C")
 
 class Fields(UserDict):
     def __init__(self, key_to_limit: Dict[str, int], default_limit=500):
@@ -28,19 +32,19 @@ class Fields(UserDict):
         self.default_limit = default_limit
         self.data = {}
         
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: str):
         limit = self.key_to_limit[key] if key in self.key_to_limit else self.default_limit
         if len(value) > self.default_limit:
             raise ExceedError(f"{key} exceeds {limit - len(value)}.")
-        self.data[key] = value
+        self.data[key] = but_control_char(value)
 
 def save_bitwarden_csv(recs: List[TsRecord], fn: str):
     # from attrdict import AttrDict
     fieldnames = BITWARDEN_CSV_STR.split(',')
     with open(fn, 'w') as f:
         wtr = csv.DictWriter(f, fieldnames=fieldnames)
-        headers = BITWARDEN_CSV_STR.split(',')
-        wtr.writerow(headers)
+        # headers = BITWARDEN_CSV_STR.split(',')
+        wtr.writerow(BITWARDEN_FIELDNAMES_DICT)
         fields = Fields({'notes': 5000})
         for rec in recs:
             # adic = AttrDict(ric)
@@ -55,13 +59,14 @@ def save_bitwarden_csv(recs: List[TsRecord], fn: str):
                 fields['folder'] = folder
                 fields['favorite'] = ''
                 fields['type'] = 'login'
-                fields['name'] = rec.title
-                fields['notes'] = expand_s(rec.notes)
-                fields['fields'] = expand_fields(rec.custom_fields)
-                fields['login_url'] = rec.login_node_url
-                fields['login_username'] = rec.login
-                fields['login_password'] = rec.password
-                fields['login_totp'] = rec.totp #  'TFC:Keeper'
+                fields['name'] = rec.title or ''
+                fields['notes'] = expand_s(rec.notes) if rec.notes else ''
+                fields['fields'] = expand_fields(rec.custom_fields) if rec.custom_fields else ''
+                fields['login_uri'] = rec.login_node_url if rec.login_node_url else ''
+                fields['login_username'] = rec.login if rec.login else ''
+                fields['login_password'] = rec.password if rec.password else ''
+                fields['login_totp'] = rec.totp if rec.totp else '' #  'TFC:Keeper'
+                breakpoint()
                 wtr.writerow(fields)
             except ExceedError: # as err:
                 pprint(rec.to_dictionary())
@@ -76,11 +81,15 @@ def load_keeper_records(fn: str) -> List[Dict]:
 def expand_s(s: str):
     return s.replace(r'\n', '\n')
 
-def expand_fields(d: Dict) -> List[str]:
-    if not len(d):
+def expand_fields(il: List) -> str:
+    if not len(il):
         return []
-    lst = [k + ': ' + v for k, v in d.items()]
-    return '\n'.join(lst)
+    ol = []
+    for dic in il:
+        if dic['type'] != 'text':
+            raise ValueError(f"{dic['type']} is not supported.")
+        ol.append(f"{dic['name']}:{dic['value']}")
+    return '\n'.join(ol)
 
 def list_all_records(sss: KeeperSession):
     return [TsRecord(sss[uid]) for uid in sss.get_every_uid()]
