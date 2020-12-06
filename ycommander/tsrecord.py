786013f02c4ad5a73314f421376568e1fde8b4aa
MAKINO Yasukazu <yasukazu@pm.me>
2020-12-06 15:42:10 +0900

    classmethod new_with_timestamp(cls, udi, keeperSession)

--- ycommander/tsrecord.py
+++ ycommander/tsrecord.py
@@ -50,6 +50,13 @@ class TsRecord(Record):
         tsr.timestamp = timestamp
         return tsr
 
+    from .session import KeeperSession
+    @classmethod
+    def new_with_timestamp(cls, uid: Uid, ks: KeeperSession):
+        rec = ks.get_record_with_timestamp()
+        tsr = TsRecord(rec)
+        return tsr
+
     @cached_property
     def login_url_components(self) -> NamedTuple:
         '''
